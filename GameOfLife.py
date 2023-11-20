66b6be8e98b541d5ac38765637c754aaaf0685fb
Ahmad Jebran <ahmad.jebran@students.snow.edu>
2023-11-20 15:27:04 -0700

    working on the second fucnion

--- GameOfLife.py
+++ GameOfLife.py
@@ -13,11 +13,17 @@ def Get_living_Cells(cells, numRows, numCol):
  
 
 print(Get_living_Cells(cells,4,3))
+# Write a python function that accepts a numpy array representing the current board and 
+# returns a numpy representing the next generation of living cells (assume that cells out of bounds of the board immediately die).  
+# (Don't use any loops here!)
 
 def GetNextGeneration(cells):
-    neighbors = [range(-1,1), range(-1,1)]
-    if(cells[0:cells.shape[0],0:cells.shape[1]].any == 1):
-        print("hit!")
+    result = (
+        cells[0:cells.shape[0]-1, 0:cells.shape[1]-1] +
+        cells[0:cells.shape[0], 0:cells.shape[1]-1] +
+        cells[0:cells.shape[0]-1, 0:cells.shape[1]-1]
+    )
+    return result
 
 GetNextGeneration(Get_living_Cells(cells, 4,3))
 
