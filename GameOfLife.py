b347b32d938c70c3e879c60e9751fe995aeafe14
CarlosBlanco1 <97683431+CarlosBlanco1@users.noreply.github.com>
2023-11-27 23:49:33 -0700

    99% done

--- GameOfLife.py
+++ GameOfLife.py
@@ -9,7 +9,6 @@ def Get_living_Cells(cells, numRows, numCol):
     for x in cells:
         life_grid[x[0],x[1]] = 1
 
-    # print(life_grid)
     return life_grid
  
 
@@ -40,20 +39,56 @@ def GetNextGeneration(cells):
     counts[down_right] += cells[up_left]
     counts[up_left] += cells[down_right]
 
-    will_live = (counts == 2)&(cells == 1 ) | (cells == 1)
-    # TODO
-    # create 2D array of arrays like the first structure
+    # A live cell dies if it has fewer than two live neighbors.
+    # A live cell with two or three live neighbors lives on to the next generation.
+    # A live cell with more than three live neighbors dies.
+    # A dead cell will be brought back to live if it has exactly three live neighbors.
+
+    will_live = (counts == 2)&(cells == 1 ) | (counts == 3)&(cells == 1) |(counts == 3)&(cells == 0)
 
     print(counts)
-    return will_live
+    print(will_live)
 
-    # result = (
-    #     cells[0:cells.shape[0]-1, 0:cells.shape[1]-1] +
-    #     cells[0:cells.shape[0], 0:cells.shape[1]-1] +
-    #     cells[0:cells.shape[0]-1, 0:cells.shape[1]-1]
-    # )
-    # return result
+    # TODO
+    # create 2D array of arrays like the first structure
+    next_generation = will_live.astype(int)
+    print(np.array(next_generation))
 
-GetNextGeneration(Get_living_Cells(cells, 4,3))
+    return np.array(next_generation)
 
+
+#### TESTS #####
+assert Get_living_Cells({(2,0),(2,1),(2,2)}, 5, 3).tolist() == [[0,0,0],[0,0,0],[1,1,1],[0,0,0],[0,0,0]]
+assert Get_living_Cells({(0,1),(1,1),(2,1)}, 4, 2).tolist() == [[0,1],[0,1],[0,1],[0,0]]
+
+assert np.array_equal(GetNextGeneration(np.array([[0,0,0],[0,0,0],[1,1,1],[0,0,0],[0,0,0]])), np.array([[0,0,0],[0,1,0],[0,1,0],[0,1,0],[0,0,0]]))
+assert np.array_equal(GetNextGeneration(np.array([[0,0,0],[0,1,0],[0,1,0],[0,1,0],[0,0,0]])), np.array([[0,0,0],[0,0,0],[1,1,1],[0,0,0],[0,0,0]]))
+
+
+import turtle
+
+array_to_draw = GetNextGeneration(Get_living_Cells(cells, 5,3))
+
+def draw_array (array):
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            turtle.goto(50 * j, i * 50)
+            turtle.pendown()
+
+            if array[i, j] == 1:
+                turtle.begin_fill()
+            turtle.forward(50)
+            turtle.right(90)
+            turtle.forward(50)
+            turtle.right(90)
+            turtle.forward(50)
+            turtle.right(90)
+            turtle.forward(50)
+            turtle.right(90)
+            turtle.forward(50)
+            if array[i, j] == 1:
+                turtle.end_fill()
+
+            turtle.penup()
+    turtle.done()
 
